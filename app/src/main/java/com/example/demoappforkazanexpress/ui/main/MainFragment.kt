048a0cda6c7fc495790387b27b56d376d89d6d9a
ShamsutOFF <shamsutoff@yandex.ru>
2021-08-31 11:13:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/demoappforkazanexpress/ui/main/MainFragment.kt
@@ -0,0 +1,26 @@
+package com.example.demoappforkazanexpress.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.demoappforkazanexpress.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+}
